c584242d68e29eb93f6e3a9fdd2af8bb1430e919
flaviorv <frezendevaroto@gmail.com>
2025-05-29 08:16:49 -0300

    Verifying the gap between train and test. If it is high, there is possible overfit.

--- breast_cancer_filtering.py
+++ breast_cancer_filtering.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SelectKBest, f_classif, chi2, mutual_info_classif
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 
 def filtering(method, features, target, k=5, cv=5):
     selector = SelectKBest(score_func=method, k=k)
@@ -12,15 +12,19 @@ def filtering(method, features, target, k=5, cv=5):
 
 def k_folds(features, target, cv, mask=[]):
     model = RandomForestClassifier()
-    scores = cross_val_score(model, features, target, cv=cv)
+    scores = cross_validate(model, features, target, cv=cv, return_train_score=True, scoring='accuracy')
     if len(mask) > 0:
         print('Selected features:', data.feature_names[mask])
-    print('Accuracy mean:', round(scores.mean(), 2), '\n')
+    train_mean = scores['train_score'].mean()
+    test_mean = scores['test_score'].mean()
+    print('Train mean', train_mean)
+    print('Test mean', test_mean)
+    print('Gap', train_mean - test_mean, '\n')
 
 if __name__ == '__main__':
     data = load_breast_cancer()
     features, target = data.data, data.target
-
+    
     scaler = MinMaxScaler()
     scaled = scaler.fit_transform(features)
 
@@ -29,7 +33,8 @@ if __name__ == '__main__':
     k_folds(scaled, target, 5)
 
     k = 18
-    print(f'With ANOVA, CHI2, MUTUAL INFORMATION (Best {k})')
+    print(f'With ANOVA, CHI-SQUARE, MUTUAL INFORMATION (Best {k})')
     filtering(f_classif, features, target, k)
     filtering(chi2, features, target, k)
     filtering(mutual_info_classif, features, target, k)
+    
